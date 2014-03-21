0728369683dae6f13a41ebc400eed18500758981
Ojash <ojashdahal@lftechnology.com>
2014-03-21 15:43:40 +0545

    project initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InstaPop::Application.config.secret_key_base = 'a0fce2300fe74914a9fc05605237b4e9bce73080628f3a547f6e51e0d1ae44796f37dc83a4fbfd3b1cbaa14cb18896b867e935bd8170c4e08664887ed94f9096'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InstaPop::Application.config.session_store :cookie_store, key: '_insta-pop_session'
